7f4d4e0e1fb3e188ffc0fd1d4947aaff6f80ffc5
vsb <vsb@cin.ufpe.br>
2022-09-02 09:54:59 -0300

    <feat> nest g resource client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientModule } from './client/client.module';
 
 @Module({
-  imports: [],
+  imports: [ClientModule],
   controllers: [AppController],
   providers: [AppService],
 })
