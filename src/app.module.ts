b503dfba83816afffdf086934a6ab4e7c8b65fb2
vsb <vsb@cin.ufpe.br>
2022-09-02 18:35:03 -0300

    <feat> nest g resource market

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientModule } from './client/client.module';
+import { MarketModule } from './market/market.module';
 
 @Module({
-  imports: [ClientModule],
+  imports: [ClientModule, MarketModule],
   controllers: [AppController],
   providers: [AppService],
 })
