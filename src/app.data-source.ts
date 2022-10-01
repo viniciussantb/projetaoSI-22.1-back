35b9a26fa957ac97de10c2aff9c1b069a7fb4da6
vsb <vsb@cin.ufpe.br>
2022-10-01 13:55:46 -0300

    <fix> app.data-source correction

--- src/app.data-source.ts
+++ src/app.data-source.ts
@@ -3,14 +3,9 @@ import 'dotenv/config';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: +process.env.POSTGRES_PORT!,
-  username: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
+  url: process.env.DATABASE_URL,
   synchronize: false,
-  logging: !!process.env.POSTGRES_LOGGING,
-  entities: [process.env.POSTGRES_ENTITIES!],
-  migrations: [process.env.POSTGRES_MIGRATIONS!],
-
-})
+  logging: false,
+  entities: ['./dist/**/*.entity.js'],
+  migrations: ['./src/database/migrations'],
+});
