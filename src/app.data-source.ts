c49e0afce0ae84011b79f9566a9565ec66a827ec
vsb <vsb@cin.ufpe.br>
2022-10-01 15:10:32 -0300

    <fix> ssl correction

--- src/app.data-source.ts
+++ src/app.data-source.ts
@@ -4,6 +4,9 @@ import 'dotenv/config';
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
   synchronize: false,
   logging: false,
   entities: ['./dist/**/*.entity.js'],
