b6ecaffb46506ad3ef2fe4cc1bc623bf6c2d009f
vsb <vsb@cin.ufpe.br>
2022-10-12 19:44:55 -0300

    <fix> notification file name

--- src/notification/entities/clientNotification.entity.ts
+++ src/notification/entities/clientNotification.entity.ts
@@ -8,16 +8,21 @@ import {
 } from "typeorm";
 import { ApiProperty, ApiTags } from "@nestjs/swagger";
 import { Category } from "../../category/entities/category.entity";
+import { Client } from "../../client/entities/client.entity";
 
-@ApiTags('MarketNotification')
-@Entity({ name: 'marketNotification' })
-export class MarketNotification {
+@ApiTags('ClientNotification')
+@Entity({ name: 'clientNotification' })
+export class ClientNotification {
   @ApiProperty({ name: 'id', type: Number })
   @PrimaryGeneratedColumn()
   id!: number;
 
+  @ApiProperty({ name: 'client', type: Client, nullable: true })
+  @ManyToOne(() => Client, (client) => client.clientNotification)
+  client?: Client;
+
   @ApiProperty({ name: 'category', type: Category })
-  @ManyToOne(() => Category, (category) => category.marketNotification)
+  @ManyToOne(() => Category)
   category?: Category;
 
   @ApiProperty({ name: 'neighborhood', type: String, default: true })
--- src/notification/entities/marketNotification.enitity.ts
+++ src/notification/entities/marketNotification.enitity.ts
@@ -8,21 +8,16 @@ import {
 } from "typeorm";
 import { ApiProperty, ApiTags } from "@nestjs/swagger";
 import { Category } from "../../category/entities/category.entity";
-import { Client } from "../../client/entities/client.entity";
 
-@ApiTags('ClientNotification')
-@Entity({ name: 'clientNotification' })
-export class ClientNotification {
+@ApiTags('MarketNotification')
+@Entity({ name: 'marketNotification' })
+export class MarketNotification {
   @ApiProperty({ name: 'id', type: Number })
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @ApiProperty({ name: 'client', type: Client, nullable: true })
-  @ManyToOne(() => Client, (client) => client.clientNotification)
-  client?: Client;
-
   @ApiProperty({ name: 'category', type: Category })
-  @ManyToOne(() => Category, (category) => category.clientNotification)
+  @ManyToOne(() => Category)
   category?: Category;
 
   @ApiProperty({ name: 'neighborhood', type: String, default: true })
